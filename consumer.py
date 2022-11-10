3fb2bc6b482320a216dd7a74f148d410526ac8ea
DILEESH-PV <dileeshpv1998@gmail.com>
2022-11-10 10:36:22 +0530

    first commit

---
+++ consumer.py
@@ -0,0 +1,27 @@
+while True:
+    print("\nSelect an option")
+    print("1 Add a consumer")
+    print("2 Search a consumer")
+    print("3 Delete a consumer")
+    print("4 Update a consumer")
+    print("5 view all consumer")
+    print("6 Generate bill")
+    print("7 View bill")
+    print("8 exit")
+    ch=int(input("Select an option  : \n"))
+    if (ch==1):    
+        print("Selected add a consumer")
+    elif(ch==2):
+         print("Selected search a consumer")
+    elif(ch==3):
+        print("Selected delete a consumer")
+    elif(ch==4):
+        print("Selected update a consumer")
+    elif(ch==5):
+        print("Selected viewall consumer")
+    elif(ch==6):
+        print("Selected generate bill")
+    elif(ch==7):
+        print("Selected view all")
+    elif(ch==8):
+        break
